620bec26727d222eeb728177c8015c2fe4f79393
quebell7 <kbell.marinescu@gmail.com>
2016-11-17 22:06:18 -0800

    some changes

---
+++ js/index.js
@@ -0,0 +1,45 @@
+$(document).ready(pageReady);
+
+function pageReady() {
+	
+
+///nav links
+
+$(".navline").hover(function showRollover() {
+		 event.preventDefault();
+    $(this).addClass("rollover");
+    // $(this).show("thickline");
+
+ }, function removeRollover() {
+ 		 event.preventDefault();
+    $(this).removeClass("rollover");
+} );
+
+
+$(".mobile-nav-button").click(mobileNavToggle);
+	
+	function mobileNavToggle() {
+		$(".mobile-nav").slideToggle();
+		$(".line").toggleClass("active");
+		$("#logo").toggle();
+		    if ($(window).width() > 768) {
+      	$(".orders").toggle();
+    	}
+	}
+
+}
+
+
+
+
+// function clickHandler() {
+//     $('#hide').toggle();
+//     $('#show').toggle();
+//     $('.orders').toggle();
+// }
+    
+// $(document).ready(function(){
+//     $('#show').hide();
+    
+//     $('#hide, #show').on('click', clickHandler);
+// });
